12190346b74d56b3a54bb54d83e80bd44747e718
CarsonMBrown <carson.mattox@gmail.com>
2022-11-16 19:38:30 +0000

    add bounding info and figures to report

--- src/binarization/thresholding/input_sensitive_threshold.py
+++ src/binarization/thresholding/input_sensitive_threshold.py
@@ -2,10 +2,10 @@ from src.util.dir_util import *
 from src.util.img_util import *
 
 
+@Warning
 def run(img_in_dir, img_out_dir):
     """
     Runs algorithm from "Input sensitive thresholding for ancient Hebrew manuscript" by Itay Bar-Yosef
-
     :param img_in_dir: glyph_path to directory containing input images
     :param img_out_dir: glyph_path to directory to output images
     :return: None
@@ -15,9 +15,6 @@ def run(img_in_dir, img_out_dir):
 
     for img_path in img_list:
         img, img_output = load_image(img_in_dir, img_out_dir, img_path, gray_scale=True)
-        # GET HISTOGRAM
-        histograph = cv2.calcHist(img, [0], None, [256], [0, 256])
-        # GET GLOBAL THRESHOLD VALUE
         # BINARIZE USING THRESHOLD
         _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
         save_image(img_output, img)
